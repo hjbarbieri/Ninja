ea6a6a74b771c11c442ac6fd8e7a8bc8fd7354f5
mobile-barbieri <hbarbieri@mobilecomputing.com.ar>
2015-07-08 17:15:01 -0300

    Add Routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,38 @@
 //angular.module('initialApp',['loginModule']);
 
-angular.module('myApp',[]).controller('GreetingController',function($scope){
-	$scope.now=new Date(); //set the model 'now' on scope
-	$scope.greeting='Hello'; //set the name model on scope
+angular.module('myApp',[]).controller('MessageController', function($scope, $timeout) {
+	$scope.messages = [{
+		sender: 'user1',
+		text: 'Message1'
+	}];
+	var timer;
+	var count=0;
+	$scope.loadMessages = function() {
+		count++;
+		$scope.messages.push({
+			sender: 'user1',
+			text: 'Random message'+count
+		});
+		timer=$timeout($scope.loadMessages, 2000);
+		if(count==3){
+			$scope.$broadcast('EVENT_NO_DATA', 'Not Connected');
+			$timeout.cancel(timer);
+		}
+	};
+	timer=$timeout($scope.loadMessages, 2000);
+	$scope.$on('EVENT_RECEIVED',function(){
+		console.log('Received emitted event');
+	});
+}).controller('StatsController'
+, function($scope) {
+	$scope.name = 'World';
+	$scope.status = 'Connected';
+	$scope.statusColor='green';
+	$scope.$on('EVENT_NO_DATA', function(event, data) {
+		console.log('received broadcasted event');
+		$scope.status = data;
+		$scope.statusColor='red';
+		$scope.$emit('EVENT_RECEIVED');
+	});
 });
+
